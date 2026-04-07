b6f62b11bdf3bc7cc3184064c43d98d4aea1b903
jonibek99 <jonibekannaev5@gmail.com>
2026-04-07 22:27:21 +0500

    solo_js_leveling

---
+++ app.js
@@ -0,0 +1,22 @@
+/*let ism= 'jonibek'
+let yosh =23 
+
+const shahar= 'Samarqandli'
+console.log(ism,yosh,'da',shahar)
+
+let name_ = 'jonibek'
+let sure_name= 'Abdurasulov'
+let last_name='aziz'
+let fullnames=name_ +' '+sure_name+' '+last_name+' '+'Ugli'
+let to_lower_case=fullnames.toLowerCase()
+let index_of=fullnames.indexOf('z')
+//console.log(fullnames)
+//console.log(fullnames)
+//console.log(fullnames.length)
+//console.log(fullnames.toUpperCase())
+//console.log(to_lower_case)
+
+console.log(index_of)*/
+
+
+
