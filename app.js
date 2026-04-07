e92ae350d33f0079f14e7f38c694c1b751845fbb
jonibek99 <jonibekannaev5@gmail.com>
2026-04-07 22:31:50 +0500

    solo_js_leveling

--- app.js
+++ app.js
@@ -19,4 +19,51 @@ let index_of=fullnames.indexOf('z')
 console.log(index_of)*/
 
 
+// For js string methods
+
+/*
+// // length -> uzunlikni beradi
+"Salom".length // 5
+
+// toUpperCase() -> katta harf qiladi
+"salom".toUpperCase() // "SALOM"
+
+// toLowerCase() -> kichik harf qiladi
+"SALOM".toLowerCase() // "salom"
+
+// trim() -> bosh/oxir bo‘sh joyni o‘chiradi
+" salom ".trim() // "salom"
+
+// slice(start,end) -> qism kesib oladi
+"JavaScript".slice(0,4) // "Java"
+
+// replace(a,b) -> birinchi a ni b ga almashtiradi
+//"Hello world".replace("world","JS") // "Hello JS"
+
+// includes() -> ichida borligini tekshiradi
+//"JavaScript".includes("Script") // true
+
+// startsWith() -> boshlanishini tekshiradi
+//"JavaScript".startsWith("Java") // true
+
+// endsWith() -> tugashini tekshiradi
+//"JavaScript".endsWith("Script") // true
+
+// indexOf() -> indexini topadi
+"JavaScript".indexOf("S") // 4
+
+// split() -> arrayga bo‘ladi
+"A,B,C".split(",") // ["A","B","C"]
+
+// concat() -> qo‘shadi
+"Hello ".concat("JS") // "Hello JS"
+
+// charAt() -> harfni qaytaradi
+"JS".charAt(0) // "J"
+
+// repeat() -> takrorlaydi
+"Hi ".repeat(2) // "Hi Hi "//
+*/
+
+
 
