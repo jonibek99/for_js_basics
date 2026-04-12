f5bea16cbb6238092f3d015f732aaaf4d9985599
jonibek99 <jonibekannaev5@gmail.com>
2026-04-12 19:28:14 +0500

    solo

--- if_statesments/app.js
+++ if_statesments/app.js
@@ -2,5 +2,11 @@
 const mehonlar=['aziz','jonibek','hakimali','laziz']
 // let me=[]
 // for (let i=0; mehonlar[i]=mehonlar[0].toUpperCase();me.push(mehonlar)){console.log(me)}
+ let i='123456'
+ if (i.length>=12){console.log('im too big')
 
-let result=mehonlar.map(mehonlar=>name[0].to)
+ } else if (i.length>6){
+    console.log('hallo im big')
+ } else{
+    console.log(' hello im small')
+ }
