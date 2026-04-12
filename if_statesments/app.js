e0f49fd2205c0e98358a50e34a05f1b09285ddfc
jonibek99 <jonibekannaev5@gmail.com>
2026-04-12 18:29:31 +0500

    solo

---
+++ if_statesments/app.js
@@ -0,0 +1,6 @@
+// let =25
+const mehonlar=['aziz','jonibek','hakimali','laziz']
+// let me=[]
+// for (let i=0; mehonlar[i]=mehonlar[0].toUpperCase();me.push(mehonlar)){console.log(me)}
+
+let result=mehonlar.map(mehonlar=>name[0].to)
---
+++ while&doWhile/app.js
@@ -0,0 +1,27 @@
+let names=['sardor','ogabek','rustam']
+// let i=0
+
+
+// while(i<names.length){
+//     console.log(names[i].toUpperCase())
+//     i++
+// }
+
+
+
+
+// for (let i=0;i<names.length;i++){names[i]=names[i].toUpperCase
+//     console.log(names[i])
+// }let names=['sardor','ogabek','rustam']
+// let i=0
+// while(i<names.length){
+//     console.log(names[i].toUpperCase())
+//     i++
+// }
+
+// for (let i=0; i<names.length;i++){names[i]=names[i].toUpperCase()}
+// console.log(names)
+
+
+let i=0
+while (i<names.length){console.log(names[i].toUpperCase(),i++)}
