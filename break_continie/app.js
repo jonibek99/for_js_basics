003e841c54a33136f1aeb25de21d52225b8c43ab
jonibek99 <jonibekannaev5@gmail.com>
2026-04-15 15:57:01 +0500

    solo

---
+++ break_continie/app.js
@@ -0,0 +1,11 @@
+let a=[1,2,3,4,5,6,7,8,9]
+
+
+
+
+for (let i=0;i<a.length;i++){
+    if (a[i]==3){
+        console.log(a[i])
+       break
+    }
+}
---
+++ swith/app.js
@@ -0,0 +1,24 @@
+// let a=[1,2,3,4,5,6,7,8,9]
+
+
+let baho=String(prompt('enter'))
+
+switch (baho){
+    case "A": 
+    console.log('sizning bahoyingiz A') 
+    break
+    case 'B':
+        console.log("sizning bahoyingiz B") 
+        break
+    case 'C':
+         console.log("sizning bahoyingiz C")
+         break
+    case "D": 
+    console.log("sizning bahoyingiz D")
+    break
+    case "G": 
+    console.log("sizning bahoyingiz G")
+    break
+    default: 
+ console.log('sizning bahoyingiz 0')
+}
