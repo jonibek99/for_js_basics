af58b247f2a04fc27e984d0ec4b4aac56be45eee
jonibek99 <jonibekannaev5@gmail.com>
2026-05-15 15:21:21 +0500

    solo

--- js_practivce/app.js
+++ js_practivce/app.js
@@ -4,55 +4,62 @@ let num=Number(prompt('enter your number'))
 // if (num%2==0){console.log('Juft')} else{console.log("toq")}.  // 1 - Masala
 
 
-//2 masala harorat
-
-// if(num<0){
+// 2 masala harorat
+// const My_Fucktion=(value)=>{
+// if(value<0){
 //     console.log('juda sovuq')
-// } else if(num >0 & num<20){
+// } else if(value >0 && value<20){
 //     console.log('Salqin')
-// } else if( num>20 & num<35){
+// } else if( value>20 && value<35){
 //     console.log('ILliq/Issiq')
 // } else{
 //     console.log('Juda issiq')
 // }
+// }
+// My_Fucktion(num)
+
 
 
 //3 masala kabisa yili
-
-// if(num%4==0 & num%100!=0 || num %400==0){
+// const Cabisa_yili=(val)=>{
+// if(val%4==0 && val%100!=0 || val %400==0){
 //     console.log('Kabisa yili')
 // } else{
 //     console.log('Kabisa mas')
 // }
+// }
+// Cabisa_yili(num)
 
 //4 masala baholash tizimi
+
+// const baholash_tizim=(val)=>{
 // switch (true) {
-//     case num <60:
+//     case val <60:
 //         console.log('F');
 //         break;
-//     case num >60 && num<70:
+//     case val >=60 && val<70:
 //         console.log('D');
 //         break;
-//     case num>=70 && num<80:
+//     case val>=70 && val<80:
 //         console.log('C');
 //         break;
-//     case num>=80 && num <90:
+//     case val>=80 && val <90:
 //         console.log('B');
 //         break;
-//     case num>=90 && num<100:
+//     case val>=90 && val<100:
 //         console.log('A');break;
 //     default:
 //         console.log('notgri ball');
     
 // }
+// }
+// baholash_tizim(num)
 
 
 //5 masala 3 ta sondan katasini topish
 
-// let a=Number(prompt('enter a'))
-// let b=Number(prompt('enter b'))
-// let c=Number(prompt('enter c'))
 
+// const kattasini_topish=(a,b,c)=>{
 // if(a>b&&a>c){
 //     console.log( a+'kattasi')
 // } else if(b>a&&b>c){
@@ -60,32 +67,43 @@ let num=Number(prompt('enter your number'))
 // } else{
 //     console.log(c+"eng kattsi")
 // }
+// }
+// let a=Number(prompt('enter a'))
+// let b=Number(prompt('enter b'))
+// let c=Number(prompt('enter c'))
+// kattasini_topish(a,b,c)
 
 // 6 masala chegirma
-// if(num>100000){
-//     let discount=num*0.10;
-//     let final_cost=num-discount;
+// const Chegurma_funck=(val)=>{val
+// if(val>100000){
+//     let discount=val*0.10;
+//     let final_cost=val-discount;
 //     console.log('chegirma ' +" "+ discount);
 //     console.log('yakuniy chegirma '+" "+ final_cost);
 // } else{
 //     console.log('chegirma yuq');
-//     console.log('yakuniy tulov '+" "+ num)
+//     console.log('yakuniy tulov '+" "+ val)
 // }
-//7 masala yosh chegarasi
+// }
+// Chegurma_funck(num)
 
-// if (num>=18){
+
+//7 masala yosh chegarasi
+// const Yosh_Masalasi=(val)=>{
+// if (val>=18){
 //     console.log('Ruxsat etildi')
 // }else{
 //     console.log('Kattalar bn kirishingiz mumkin')
 // }
-
+// }
+// Yosh_Masalasi(num)
 
 
 //8 masala kalkulator
-
 //  let a=Number(prompt('enter a'))
 //  let b=Number(prompt('enter b'))
 //  let c=prompt('enter operator')
+// const cla_culator=(a,b,c)=>{
 
 //  switch (c){
 //     case '+':
@@ -104,9 +122,11 @@ let num=Number(prompt('enter your number'))
 //     default:
 //         console.log('Notugri amal')
 //  }
-
+// }
+// cla_culator(a,b,c)
 //9 masala kvadrat
 
+// const Kvadrat_Yuzi=(a,b)=>{
 //  let a=Number(prompt('enter a'))
 //  let b=Number(prompt('enter b'))
 //  if(a===b){
@@ -116,16 +136,21 @@ let num=Number(prompt('enter your number'))
 //  }
 //  console.log('turtburchak yuzasi '+" "+a*b)
 
+// }
+// Kvadrat_Yuzi(a,b)
+
 
 //10 masala musbat , manfir yoki 0
-
-switch(true){
-    case num>0:
-        console.log('Musbat');break;
-    case num<0:
-        console.log('Manfiy');break;
-    default:
-        console.log('Nol')
-}
+// const Calculator=(val)=>{
+// switch(true){
+//     case val>0:
+//         console.log('Musbat');break;
+//     case val<0:
+//         console.log('Manfiy');break;
+//     default:
+//         console.log('Nol')
+// }
+// }
+// Calculator(num)
 
 
